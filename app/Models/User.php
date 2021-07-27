c2778396bbef1463738281abef7b4b35fc4651ce
moiseswar <moises12201323@gmail.com>
2021-07-27 15:43:00 -0500

    Coreccion de relaciones DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function semillas(){ 
+        return $this->hasMany('App/Models/semillas');
+    }
 }
